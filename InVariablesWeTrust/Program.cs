b4afb6c59bc9d3e8a32275751bbcb0fd8466fbe4
teunvanderploeg <54798753+teunvanderploeg@users.noreply.github.com>
2019-10-03 11:25:35 +0200

    even een groote commit met van ales

---
+++ InVariablesWeTrust/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace in_variables_we_trust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char eenA = 'A';
+            float flieg = 10;
+            bool ja = true;
+            bool nee = false;
+            int leven = 0;
+            int dagen = 100;
+            int jaar = 365;
+            string beginer = "welkom beginer";
+            string hetBegin = "welkom bij het begin";
+            string naam = hetBegin + beginer;
+            Console.WriteLine(eenA);
+
+            Console.WriteLine(flieg);
+
+            Console.WriteLine(ja);
+            Console.WriteLine(nee);
+
+            Console.WriteLine(leven);
+            Console.WriteLine(dagen);
+            Console.WriteLine(jaar);
+            Console.WriteLine(dagen - jaar);
+
+            Console.WriteLine(beginer);
+            Console.WriteLine(hetBegin);
+            Console.WriteLine(naam);
+        }
+    }
+}
---
+++ What's_my_name/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam;
+            string woonplaats;
+            string kleuroog;
+            string postkcode;
+            Console.Clear();
+            Console.WriteLine("Hallo ik ben Teun.");
+            Console.WriteLine("Wat is jou voor en achternaam?");
+            naam = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(naam + " waar woon je?");
+            woonplaats = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Wat is je de kleur van je ogen " + naam + ".");
+            kleuroog = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("En wat is je postcode?");
+            postkcode = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Clear();
+
+            Console.WriteLine("Dankje voor je meewerking.");
+            Console.WriteLine("Dit is de info dat we over je hebben.");
+            Console.WriteLine();
+            Console.WriteLine("Je naam is " + naam + ". En je oog kleur is " + kleuroog + ".");
+            Console.WriteLine("Je woont in " + woonplaats + ". En je postcode is " + postkcode + ".");
+            Console.ReadLine();
+        }
+    }
+}
